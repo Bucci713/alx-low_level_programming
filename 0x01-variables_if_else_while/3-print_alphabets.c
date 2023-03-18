8b9412217d41200c8f909c6ae00a48d64b34f26c
Bucci713 <chimbuchialpho@gmail.com>
2023-03-17 17:15:55 -0700

    alpaha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,15 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
+	ch = a;
+	
+	ch <= z;
+
+	putchar("ch");
+
+	ch++
+
+	putchar("/n")
+
 	return (0);
 }
